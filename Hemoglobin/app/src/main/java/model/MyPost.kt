d48032296b688939277a85d663a59f084baf00de
EunsuSeo01 <eunsu0613@naver.com>
2023-05-19 23:53:00 +0900

    Feat : 마이페이지의 내 글 목록 RecyclerView Adapter 설정

---
+++ Hemoglobin/app/src/main/java/model/MyPost.kt
@@ -0,0 +1,12 @@
+package model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+// 내 글 데이터 객체
+data class MyPost(
+    val upload_date: String,
+    val content: String,
+    val commentNum: Int = 0
+): Parcelable
