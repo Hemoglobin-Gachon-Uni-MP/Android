ba13f15d9b91da6bbe126efaa32d8cb92d2afb5b
dobby <dobbidd0@gmail.com>
2023-05-20 21:05:10 +0900

    name change

---
+++ Hemoglobin/app/src/main/java/com/pline/src/main/home/FragmentNewPostProvider.kt
@@ -0,0 +1,8 @@
+package com.pline.src.main.home
+
+import com.pline.R
+import com.pline.config.BaseFragment
+import com.pline.databinding.FragmentNewPostProviderBinding
+
+class FragmentNewPostProvider: BaseFragment<FragmentNewPostProviderBinding> (FragmentNewPostProviderBinding::bind, R.layout.fragment_new_post_provider) {
+}
---
+++ Hemoglobin/app/src/main/java/com/pline/src/main/home/FragmentPostReceiver.kt
@@ -0,0 +1,8 @@
+package com.pline.src.main.home
+
+import com.pline.R
+import com.pline.config.BaseFragment
+import com.pline.databinding.FragmentPostListReceiverBinding
+
+class FragmentPostReceiver :BaseFragment<FragmentPostListReceiverBinding>(FragmentPostListReceiverBinding::bind, R.layout.fragment_post_list_receiver) {
+}
